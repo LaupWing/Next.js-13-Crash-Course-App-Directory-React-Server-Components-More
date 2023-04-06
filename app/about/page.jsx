0bcf56790102582de33393c86b461e376eed1dec
LaupWing <loc_strict@hotmail.com>
2023-04-06 17:35:44 +0200

    udpate

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,9 @@
 const AboutPage = () => {
-   return <div>AboutPage</div>
+   return (
+      <div>
+         <h1>About Traversy Media</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate delectus impedit eaque nihil, quidem corporis voluptatum consequatur vel tenetur illum, fugiat maiores deserunt deleniti id doloremque, similique dignissimos minima eum!</p>
+      </div>
+   )
 }
 export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,9 @@
+const TeamPage = () => {
+   return (
+      <div>
+         <h1>Our Team</h1>
+         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam non quo ea sed, corporis ullam deleniti dolorem fugit exercitationem aliquid vitae doloremque eum suscipit eos laborum modi molestiae rem velit?</p>
+      </div>
+   )
+}
+export default TeamPage
