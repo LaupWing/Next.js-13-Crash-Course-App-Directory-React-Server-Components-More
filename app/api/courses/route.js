63474b2e74e607e54f7ce465ba4952b00bde7754
LaupWing <loc_strict@hotmail.com>
2023-04-20 05:00:30 +0200

    udpate

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -4,3 +4,14 @@ import courses from "./data.json"
 export async function GET(request) {
    return NextResponse.json(courses)
 }
+
+export async function POST(request) {
+   const { title, description, level, link} = await request.json()
+   // course.push({
+   //    title,
+   //    description
+   // })
+   return NextResponse.json({
+      message: "Course created"
+   })
+}
